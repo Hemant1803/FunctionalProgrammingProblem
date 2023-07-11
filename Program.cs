c535aae5c69fca66c67c0e950e2b2b7ecd547308
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-11 18:47:53 +0530

    Function Programming Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgrammingProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functional Programming Problems");
+        }
+    }
+}
