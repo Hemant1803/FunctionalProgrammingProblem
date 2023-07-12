d9bfdafc1ecd027572cd9ba6da702c5e707059d4
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-12 18:00:53 +0530

    Leap year

---
+++ Leapyear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class Leapyear
+    {
+        public void leapyear()
+        {
+            Console.WriteLine("Enter a Four digit Year");
+           int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("it is leap year" + year);
+            }
+            else
+            {
+                Console.WriteLine("it is not a leap year" + year);
+            }
+        }
+    }
+}
