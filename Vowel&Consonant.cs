03b2a09a91437a6fc7ab52a9c688cd70d1ebe1bd
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-14 21:47:28 +0530

    Vowel and Consonant

---
+++ Vowel&Consonant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class Vowel_Consonant
+    {
+        public void vowelconsonant()
+        {
+
+            Console.WriteLine("enter a alphabet");
+            char alphabet = Convert.ToChar(Console.ReadLine());
+
+            switch (alphabet)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is not a vowel");
+                    break;
+            }
+        }
+    }
+}
