bbb070707d710b804492b6e80671376777be5edf
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-14 20:49:08 +0530

    Swap Two Numbers

---
+++ Swap2Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class Swap2Numbers
+    {
+        public void Swap2Number() 
+        {
+            Console.WriteLine("Write 1st Number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());   
+            Console.WriteLine("Write 2nd Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+           
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            Console.WriteLine("Value After swaping");
+            Console.WriteLine("Number1 :" + num1);
+            Console.WriteLine("Number2 :" + num2);
+
+
+            
+        }
+    }
+}
