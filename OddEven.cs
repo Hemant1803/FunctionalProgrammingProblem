33100ad35b64e5e3da1efb5ddb2f740d4d7c66a8
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-14 21:02:54 +0530

    Odd Even

---
+++ OddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class OddEven
+    {
+        public void oddeven() 
+        {
+            Console.WriteLine("Enter a Number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            if(Num % 2 ==0)
+            {
+                Console.WriteLine(Num + ":- is even");              
+            }
+            else 
+            {
+                Console.WriteLine(Num + ":- is Odd");
+                    }
+        }
+    }
+}
