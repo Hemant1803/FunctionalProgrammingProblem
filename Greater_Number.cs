94aafc316387c47adf5868b88c70831a8651416d
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-14 22:05:56 +0530

    Greater Number among three

---
+++ Greater_Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    internal class Greater_Number
+    {
+        public void greater_number()
+        {
+            Console.WriteLine("Enter The 1st Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The 2nd Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The 3rd Number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                   Console.WriteLine("num1 is greater" + num1);
+                else
+                    Console.WriteLine("Number3 is greater" + num3);                
+            }
+            else if (num2 > num3)
+                Console.WriteLine("Number2 is greater" + num2);
+            else
+
+                Console.WriteLine("Number3 is greater" + num3);
+        }
+    }
+}
